64250514e24601418062fc03a3d339a9feb680f2
golubweb <darko.golubovic@hotmail.rs>
2016-04-29 23:16:54 +0200

    tabs fix

--- js/mainDirectives.js
+++ js/mainDirectives.js
@@ -1,209 +1,209 @@
 myApp.directive('audios', ['$sce', function($sce) {
-  return {
-    restrict: 'A',
-    scope: { code:'=' },
-    link: function (scope) {
-      scope.$watch('code', function (newVal, oldVal) {
-        if (newVal !== undefined) {
-          scope.url = $sce.trustAsResourceUrl("audio/" + newVal);
-        }
-      });
-    },
-    replace: true,
-    template: '<audio class="audioVoice" ng-src="{{url}}" controls></audio>'
-  };
+	return {
+		restrict: 'A',
+		scope: { code:'=' },
+		link: function (scope) {
+			scope.$watch('code', function (newVal, oldVal) {
+				if (newVal !== undefined) {
+					scope.url = $sce.trustAsResourceUrl("audio/" + newVal);
+				}
+			});
+		},
+		replace: true,
+		template: '<audio class="audioVoice" ng-src="{{url}}" controls></audio>'
+	};
 }]);
 
 myApp.directive('pagination', ['$rootScope', '$interval', '$compile', '$templateRequest', function($rootScope, $interval, $compile, $templateRequest) {
-  return {
-    restrict: 'E',
-    scope: true,
-    link: function (scope, element, attr) {
-      var promise = $interval(function(){
-        if($rootScope.pagCount !== null){
-          $interval.cancel(promise);
-          scope.pgnSize = attr.size;
-          
-          $templateRequest('partials/pagination_Board.tpl.html').then(function(html){
-            //var template = angular.element(html);
-            element.replaceWith($compile(angular.element(html))(scope));
-          });
-        }
-      }, 1000);
-    }
-  }
+	return {
+		restrict: 'E',
+		scope: true,
+		link: function (scope, element, attr) {
+			var promise = $interval(function(){
+
+				if($rootScope.pagCount !== null){
+					$interval.cancel(promise);
+					scope.pgnSize = attr.size;
+
+					$templateRequest('partials/pagination_Board.tpl.html').then(function(html){
+						element.replaceWith($compile(angular.element(html))(scope));
+					});
+				}
+			}, 1000);
+		}
+	}
 }]);
 
 myApp.directive('audioSource', [ function() {
-  return {
-    restrict: 'A',
-    scope: true,
-    link: function (scope, element, attr) {
-      element.addClass('voicePlay');
-      
-      element.on('click', function(e) {
-        var myAudio = $('.audioVoice');
+	return {
+		restrict: 'A',
+		scope: true,
+		link: function (scope, element, attr) {
+			element.addClass('voicePlay');
 
-        if( myAudio.attr('data-voice') === undefined || myAudio.attr('data-voice') !== attr.voice ){
-          myAudio.removeAttr('data-voice');
+			element.on('click', function(e) {
+				var myAudio = $('.audioVoice');
 
-          myAudio.attr({
-            'src': 'audio/' + attr.audioSource,
-            'ngSrc': 'audio/' + attr.audioSource,
-            'data-voice': attr.voice
-          });
-        }
+				if( myAudio.attr('data-voice') === undefined || myAudio.attr('data-voice') !== attr.voice ){
+					myAudio.removeAttr('data-voice');
 
-        if (!myAudio.ended && $(this).hasClass('voicePlay')) {
-          myAudio.trigger("play");
-          $(this).removeClass('voicePlay');
+					myAudio.attr({
+						'src': 'audio/' + attr.audioSource,
+						'ngSrc': 'audio/' + attr.audioSource,
+						'data-voice': attr.voice
+					});
+				}
 
-        } else{
-          myAudio.trigger("pause");
-          $(this).addClass('voicePlay');
-        }
-      });
-      //myAudio.paused && myAudio.currentTime > 0 && !myAudio.ended
-    }
-  };
+				if(!myAudio.ended && $(this).hasClass('voicePlay')) {
+					myAudio.trigger("play");
+					$(this).removeClass('voicePlay');
+
+				} else{
+					myAudio.trigger("pause");
+					$(this).addClass('voicePlay');
+				}
+			});
+			//myAudio.paused && myAudio.currentTime > 0 && !myAudio.ended
+		}
+	};
 }]);
 
 myApp.directive('searchForm', ['$compile', '$templateRequest', 'storageFactory', function($compile, $templateRequest, storageFactory){
-  return {
-    restrict: 'A',
-    controller: function($scope) {
-      $scope.days = [];
-      $scope.agent = [];
-      $scope.score = [];
-      $scope.duration = [];
+	return {
+		restrict: 'A',
+		controller: function($scope) {
+			$scope.days = [];
+			$scope.agent = [];
+			$scope.score = [];
+			$scope.duration = [];
 
-      this.numberInput = function(elmt, attrs, tmp, evt, ul) {
-        var elem = elmt[0];
-        var regex = RegExp(attrs.numberScore);
-        var value = elem.value;
+			this.numberInput = function(elmt, attrs, tmp, evt, ul) {
+				var elem = elmt[0];
+				var regex = RegExp(attrs.numberScore);
+				var value = elem.value;
 
-        elem.addEventListener(evt, function(e){
-          $(ul).remove();
+				elem.addEventListener(evt, function(e){
+					$(ul).remove();
 
-          if (regex.test(elem.value)) {
-            if(attrs.numberScore){
-              $scope.scoreResult(elem.value);
+					if(regex.test(elem.value)) {
+						if(attrs.numberScore){
+							$scope.scoreResult(elem.value);
 
-            } else if(attrs.numberDuration) {
-              $scope.durationResult(elem.value);
+						} else if(attrs.numberDuration) {
+							$scope.durationResult(elem.value);
 
-            } else {
-              $scope.deysResult(elem.value);
-            }
-            
-            if(value.length == 0){
-              $(ul).remove();
-            }
+						} else {
+							$scope.deysResult(elem.value);
+						}
 
-            $scope.getTpl(tmp, elmt.parent());
-          }else {
-            elem.value = value;
-          }
+						if(value.length == 0){
+							$(ul).remove();
+						}
 
-        });
-      };
+						$scope.getTpl(tmp, elmt.parent());
+					}else {
+						elem.value = value;
+					}
 
-      $scope.scoreResult = function(score){
-        $scope.searchScore = JSON.parse(storageFactory.getItms());  
+				});
+			};
 
-        angular.forEach($scope.searchScore, function(item) {
-            if(parseInt(item.evaluation.totalScore) == parseInt(score)){
-              $scope.score.push(item);
-            } 
-        });
-      };
+			$scope.scoreResult = function(score){
+				$scope.searchScore = JSON.parse(storageFactory.getItms());  
 
-      $scope.durationResult = function(duration){
-        $scope.searchDuration = JSON.parse(storageFactory.getItms());  
+				angular.forEach($scope.searchScore, function(item) {
+					if(parseInt(item.evaluation.totalScore) == parseInt(score)){
+						$scope.score.push(item);
+					} 
+				});
+			};
 
-        angular.forEach($scope.searchDuration, function(item) {
-          if(parseInt(item.durationTime) <= parseInt(duration)){
-            $scope.duration.push(item);
-          } 
-        });
-      };
+			$scope.durationResult = function(duration){
+				$scope.searchDuration = JSON.parse(storageFactory.getItms());  
 
-      $scope.deysResult = function(days){
-        $scope.searchDays = JSON.parse(storageFactory.getItms());  
+				angular.forEach($scope.searchDuration, function(item) {
+					if(parseInt(item.durationTime) <= parseInt(duration)){
+						$scope.duration.push(item);
+					} 
+				});
+			};
 
-        angular.forEach($scope.searchDays, function(item) {
-          if(parseInt(item.age) <= parseInt(days)){
-            $scope.days.push(item);
-          } 
-        });
-      }
+			$scope.deysResult = function(days){
+				$scope.searchDays = JSON.parse(storageFactory.getItms());  
 
-      $scope.getTpl = function(tmpUrl, rowElem) {
-        $templateRequest(tmpUrl).then(function(html){
-          var template = angular.element(html);
+				angular.forEach($scope.searchDays, function(item) {
+					if(parseInt(item.age) <= parseInt(days)){
+						$scope.days.push(item);
+					} 
+				});
+			}
 
-          rowElem.append($compile(angular.element(html))($scope));
-        });
-      }
-    }
-  };
+			$scope.getTpl = function(tmpUrl, rowElem) {
+				$templateRequest(tmpUrl).then(function(html){
+					var template = angular.element(html);
+
+					rowElem.append($compile(angular.element(html))($scope));
+				});
+			}
+		}
+	};
 }]);
 
 myApp.directive('numberScore', [ function(){
-  return {
-    restrict: 'A',
-    require: '^searchForm',
-    link: function(scope, element, attrs, searchFormController){
-      searchFormController.numberInput(element, attrs, '../angularCall/partials/search_ScoreTotal.tpl.html', 'keyup', '.search-score');
-    }
-  }
+	return {
+		restrict: 'A',
+		require: '^searchForm',
+		link: function(scope, element, attrs, searchFormController){
+			searchFormController.numberInput(element, attrs, '../angularCall/partials/search_ScoreTotal.tpl.html', 'keyup', '.search-score');
+		}
+	}
 }]);
 
 myApp.directive('numberDuration', [ function(){
-  return {
-    restrict: 'A',
-    require: '^searchForm',
-    link: function(scope, element, attrs, searchFormController){
-      searchFormController.numberInput(element, attrs, '../angularCall/partials/search_Duration.tpl.html', 'keyup', '.search-duration');
-    }
-  }
+	return {
+		restrict: 'A',
+		require: '^searchForm',
+		link: function(scope, element, attrs, searchFormController){
+			searchFormController.numberInput(element, attrs, '../angularCall/partials/search_Duration.tpl.html', 'keyup', '.search-duration');
+		}
+	}
 }]);
 
 myApp.directive('numberDays', [ function(){
-  return {
-    restrict: 'A',
-    require: '^searchForm',
-    link: function(scope, element, attrs, searchFormController){
-      searchFormController.numberInput(element, attrs, '../angularCall/partials/search_Days.tpl.html', 'keyup', '.search-days');
-    }
-  }
+	return {
+		restrict: 'A',
+		require: '^searchForm',
+		link: function(scope, element, attrs, searchFormController){
+			searchFormController.numberInput(element, attrs, '../angularCall/partials/search_Days.tpl.html', 'keyup', '.search-days');
+		}
+	}
 }]);
 
 myApp.directive('onKeydown', ['$compile', '$templateRequest', function($compile, $templateRequest){
-  return {
-    restrict: 'A',
-    scope: true,
-    controller: 'searchCtrl',
-    link: function(scope, elem, attr, searchCtrl){
-      var agentName = '';
-      var searchWrap = $('.search-wrap');
+	return {
+		restrict: 'A',
+		scope: true,
+		controller: 'searchCtrl',
+		link: function(scope, elem, attr, searchCtrl){
+			var agentName = '';
+			var searchWrap = $('.search-wrap');
 
-      elem.bind("keyup", function (event) {
-        $(searchWrap.selector).remove();
-        agentName = event.target.value;
+			elem.bind("keyup", function (event) {
+				$(searchWrap.selector).remove();
+				agentName = event.target.value;
 
-        if(agentName.length >= 3){
-          searchCtrl.agentResult(agentName);
+				if(agentName.length >= 3){
+					searchCtrl.agentResult(agentName);
 
-          $templateRequest('/partials/search_AgentID.tpl.html').then(function(html){
-            var template = angular.element(html);
+					$templateRequest('/partials/search_AgentID.tpl.html').then(function(html){
+						var template = angular.element(html);
 
-            elem.parent().append($compile(template)(scope));
-          });         
-        } else {
-          $(searchWrap.selector).remove();
-        }
-      });
-    }
-  }
+						elem.parent().append($compile(template)(scope));
+					});
+				} else {
+					$(searchWrap.selector).remove();
+				}
+			});
+		}
+	}
 }]);
