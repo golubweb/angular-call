17475787ab3a8d9f12240c0516b7df05afe7bc33
golubweb <darko.golubovic@hotmail.rs>
2016-05-02 02:07:49 +0200

    fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,21 +2,22 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		
+		concat: {
+			dist: {
+				src: ['js/app.js', 'js/mainCtrl.js', 'js/mainDirectives.js', 'js/mainFactory.js', 'js/mainFilters.js', 'js/mainServices.js'],
+				dest: 'js/main,js'
+			}
+		},
 
 		cssmin: {
 			combine: {
 				files: {
-					'css/style.min.css': ['css/style.css']
+					'css/style.min.css': ['css/style.css', 'css/responsiv.css']
 				}
 			}
-		},
-
-		concat: {
-			dist: {
-				src: ['js/app.js', 'js/mainCtrl.js', 'js/mainDirectives.js', 'js/mainFactory.js', 'js/mainFilters.js', 'js/mainServices.js'],
-				dest: 'js/main.js'
-			}
 		}
+		
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
